105f2406767e21263f7e5b5d824926334c4b65d7
Vu Thanh Nam <thanhnamvu@coin98.finance>
2022-10-06 17:56:07 +0700

    add hashService + signMessage

--- provider/solana/solanaServices.js
+++ provider/solana/solanaServices.js
@@ -1,3 +1,6 @@
+import { get } from "lodash";
+import { HashService } from "./hashService";
+
 export class SolanaService {
   static async isAddressInUse(connection, address) {
     const programInf = await connection.getAccountInfo(address, 'confirmed');
@@ -7,5 +10,29 @@ export class SolanaService {
   static async isAddressAvailable(connection, address) {
     const programInf = await connection.getAccountInfo(address, 'confirmed');
     return programInf === null;
+  }
+
+  static hashMessage = (message) => {
+    const buffer = Buffer.alloc(64)
+  const span = MessageRandomLayout.encode(message, buffer)
+  const serialize = buffer.slice(0, span)
+
+  return HashService.keckka256(serialize)
+  }
+
+  static signMessage = async (adminAccount, message) => {
+    const secretKey = get(adminAccount, 'secretKey', false);
+    if (!secretKey) {
+      return await window?.coin98?.sol
+        .request({ method: 'sol_signMessage', params: [message] })
+        .then(res => {
+          const signature = get(res, 'signature');
+
+          return Buffer.from(signature)
+      })
+    }
+    const signature = await ed.sign(message, adminAccount.secretKey.slice(0, 32))
+
+    return Buffer.from(signature)
   }
 }
