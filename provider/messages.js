1e7f406d7716667421235395e427d7cc890600da
dangduoc97 <dangvanduoc1997@gmail.com>
2022-10-10 16:57:19 +0700

    [ Update ] message

--- provider/messages.js
+++ provider/messages.js
@@ -1,9 +1,17 @@
 const MESS_000 = 'Invalid recipient address'
 const MESS_001 = 'Insufficient funds'
 const MESS_002 = 'Pending transaction'
+const EXCEED_LIMIT = 'exceeds desired slippage limit'
+const INSUFFICIENT_FUNDS = 'INSUFFICIENT_FUNDS'
+const TRANSACTION_LARGE = 'transaction too large'
+const TRANSACTION_FAILED = 'transaction failed'
 
 export default {
   MESS_000,
   MESS_001,
-  MESS_002
+  MESS_002,
+  EXCEED_LIMIT,
+  INSUFFICIENT_FUNDS,
+  TRANSACTION_LARGE,
+  TRANSACTION_FAILED
 }
